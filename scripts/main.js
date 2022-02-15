663b48b03db3724acc073be10a0fe3b846208774
Ariel Flores <emmacornio@protonmail.com>
2022-02-14 19:01:42 -0600

    Adds sound and final functionality

--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,26 @@ const showCatBtn = document.getElementById("showCatBtn");
 const changeCatBtn = document.getElementById("changeCatBtn");
 const cat = document.getElementById("cat");
 const catImage = document.getElementsByClassName("cat-image");
+const meowSound = document.getElementById("meowSound");
+
+window.addEventListener("load", () => {
+	cat.classList.toggle("show");
+});
 
 showCatBtn.addEventListener("click", () => {
-	if (cat.style.visibility == "visible") {
-		cat.style.visibility = "hidden";
+	if (cat.classList.contains("show")) {
 		showCatBtn.textContent = "Ver un gatito 😸";
+		cat.classList.toggle("show");
+		cat.classList.toggle("hide");
 	} else {
-		cat.style.visibility = "visible";
 		showCatBtn.textContent = "No ver un gatito 😿";
+		cat.classList.toggle("show");
+		cat.classList.toggle("hide");
 	}
 });
 changeCatBtn.addEventListener("click", () => {
 	catImage[0].src =
 		"https://cataas.com/cat/says/Feliz%20san%20Valentin?size=35&color=purple&type=md&tag=black" +
 		new Date();
+	meowSound.play();
 });
